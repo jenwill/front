2f09b3aa04e01d943241031bb334ecab67194ba7
Melanie Downing <sayanything830@gmail.com>
2018-03-19 09:25:25 -0700

    true false working

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -3,6 +3,7 @@ const socket = io('http://localhost:3000');
 
 let sendQuestionForm = document.getElementById('send-question-form');
 let questionInput = document.getElementById('question-input');
+let sendAnswer = document.getElementsByClassName('answer-button');
 
 sendQuestionForm.addEventListener('submit', event => {
   event.preventDefault();
@@ -13,3 +14,20 @@ sendQuestionForm.addEventListener('submit', event => {
 socket.on('RECEIVE_QUESTION', data => {
   console.log('RECEIVE_QUESTION',data);
 });
+
+sendAnswer[0].addEventListener('click', event => {
+  event.preventDefault();
+  let answer = event.target.id;
+  socket.emit('SEND_ANSWER', {answer});
+});
+
+sendAnswer[1].addEventListener('click', event => {
+  event.preventDefault();
+  let answer = event.target.id;
+  socket.emit('SEND_ANSWER', {answer});
+});
+
+socket.on('RECEIVE_ANSWER', data => {
+  console.log('RECEIVE_ANSWER',data);
+});
+
